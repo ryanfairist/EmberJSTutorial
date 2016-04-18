4f97a253251e2088bad75b57b9c10522b8d96563
RyanFairist <ryanfairistrichard@gmail.com>
2016-04-18 00:42:04 -0400

    add notes in router.js

--- app/router.js
+++ app/router.js
@@ -1,4 +1,7 @@
+// gives us access to the actual Ember.js library as the variable Ember
 import Ember from 'ember';
+//line gives us access to our app's configuration data as the variable 
+//config. const is a way to declare a read-only variable, as to make sure it is not accidentally reassigned elsewhere. 
 import config from './config/environment';
 
 const Router = Ember.Router.extend({
@@ -8,4 +11,5 @@ const Router = Ember.Router.extend({
 Router.map(function() {
 });
 
+//makes the Router variable defined in this file available to other parts of the app.
 export default Router;
